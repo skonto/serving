ade6a29202e357f6a9347f7bcd4434b323b1eeb7
Stavros Kontopoulos <skontopo@redhat.com>
2021-12-20 16:44:04 +0200

    nit

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -190,10 +190,9 @@ function install_catalogsource(){
   # And checkout the setup script based on that commit.
   local SERVERLESS_DIR=$(mktemp -d)
   local CURRENT_DIR=$(pwd)
-  git clone --depth 1 https://github.com/openshift-knative/serverless-operator.git ${SERVERLESS_DIR}
+  git clone --depth 1 --branch release-1.20 https://github.com/openshift-knative/serverless-operator.git ${SERVERLESS_DIR}
   pushd ${SERVERLESS_DIR}
 
-  git checkout release-1.20
   source ./test/lib.bash
   create_namespaces
   update_csv $CURRENT_DIR || return $?
