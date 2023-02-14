f5b8349ff04f0c5dd918ea81396de8ce6154ed49
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-02-14 18:55:58 +0900

    Bump CI template to 4.10,4.11 from 4.8,4.9 (#170)

--- openshift/ci-operator/update-ci.sh
+++ openshift/ci-operator/update-ci.sh
@@ -40,8 +40,8 @@ PERIODIC_CONFIG=$PERIODIC_CONFIGDIR/openshift-knative-serving-release-$VERSION-p
 CURDIR=$(dirname $0)
 
 # $1=branch $2=openshift $3=promotion_disabled $4=generate_continuous $5=internal_tls_enabled(optional)
-$CURDIR/generate-ci-config.sh knative-$VERSION 4.8 true false > ${CONFIG}__48.yaml
-$CURDIR/generate-ci-config.sh knative-$VERSION 4.9 true false > ${CONFIG}__49.yaml
+$CURDIR/generate-ci-config.sh knative-$VERSION 4.10 true false > ${CONFIG}__410.yaml
+$CURDIR/generate-ci-config.sh knative-$VERSION 4.11 true false > ${CONFIG}__411.yaml
 $CURDIR/generate-ci-config.sh knative-$VERSION 4.12 true false > ${CONFIG}__412.yaml
 $CURDIR/generate-ci-config.sh knative-$VERSION 4.12 false true true > ${CONFIG}__412.yaml
 
