67c19d18254edc21a9c56fc3254d9667bb5ff320
Reto Lehmann <retocode@icloud.com>
2023-03-02 10:24:36 +0100

    [RELEASE-1.8] Re-introducing https://github.com/knative/serving/pull/13337 (#179)
    * Revert "Revert "upgrade k8s autoscaling to v2 (#13337)" (#8)"
    
    This reverts commit 4abc4771ff351a59d796bf348caeb65e3a43579b.
    
    * Re-introducing 42d13773f35e4230fdf1cebe8aa6f08720c5cf9d
    
    * Run `./openshift/release/generate-release.sh`

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -21,9 +21,6 @@ git apply openshift/patches/*
 git add .
 git commit -am ":fire: Apply carried patches."
 
-# Revert the autoscaling API version change.
-git revert 974d19d03644dff46b097a15efb4d3d7167765ad
-
 make generate-dockerfiles
 make RELEASE=ci generate-release
 git add openshift OWNERS_ALIASES OWNERS Makefile
