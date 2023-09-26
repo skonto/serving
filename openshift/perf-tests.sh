585b0a019a0dec90e194b6067d3967eac61a5b8a
Stavros Kontopoulos <st.kontopoulos@gmail.com>
2023-09-26 17:49:29 +0300

    fix script path

--- openshift/perf-tests.sh
+++ openshift/perf-tests.sh
@@ -9,7 +9,7 @@ env
 failed=0
 
 (( !failed )) && install_knative || failed=1
-"$(dirname "$0")/run-all-performance-tests.sh"
+"$(dirname "$0")/performance/scripts/run-all-performance-tests.sh"
 (( failed )) && gather_knative_state
 (( failed )) && exit $failed
 
