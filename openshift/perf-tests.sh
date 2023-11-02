e8a7a11af33843c50bdb97084e1c33d656ef8abf
Stavros Kontopoulos <st.kontopoulos@gmail.com>
2023-11-02 09:29:24 +0200

    fix perf tests script

--- openshift/perf-tests.sh
+++ openshift/perf-tests.sh
@@ -10,21 +10,6 @@ failed=0
 
 git apply ./openshift/performance/patches/*
 
-go get github.com/elastic/go-elasticsearch/v7@v7.17.10
-go get github.com/opensearch-project/opensearch-go@v1.1.0
-
-./hack/update-deps.sh
-
-git add .
-git commit -m "openshift perf update"
-
-git apply ./openshift/patches/001-object.patch
-git apply ./openshift/patches/002-mutemetrics.patch
-git apply ./openshift/patches/003-routeretry.patch
-
-git add .
-git commit -m "apply reverted patches"
-
 (( !failed )) && install_knative || failed=1
 ./openshift/performance/scripts/run-all-performance-tests.sh
 (( failed )) && gather_knative_state
