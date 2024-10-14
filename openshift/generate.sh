fcb1bec61e16118f4a67682500b33dc37468e9b1
Stavros Kontopoulos <st.kontopoulos@gmail.com>
2024-10-14 11:39:35 +0300

    remove ES performance tests and patches

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -16,5 +16,18 @@ rm -rf "$tmp_dir"
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
+  --dockerfile-test-dir "ci-operator/knative-perf-images" \
+  --app-file-fmt "/ko-app/%s" \
+  --includes "test/performance/benchmarks.*" \
+  --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.17"
+
+mv "${repo_root_dir}"/openshift/images.yaml "${repo_root_dir}"/openshift/images.yaml.bak
+
+$(go env GOPATH)/bin/generate \
+  --root-dir "${repo_root_dir}" \
+  --generators dockerfile \
   --app-file-fmt "/ko-app/%s" \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.17"
+
+cat "${repo_root_dir}"/openshift/images.yaml.bak >> "${repo_root_dir}"/openshift/images.yaml
+rm "${repo_root_dir}"/openshift/images.yaml.bak
